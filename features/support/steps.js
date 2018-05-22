39bc051fd89e35c9fb7cc73e99662499f5c1ee73
warshaya <warshaya@gmail.com>
2018-05-21 22:56:39 -0700

    ~steps.js, use webdriverio in step definition, open a browser, close it at end of step

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,9 +1,16 @@
 
 const { When, Then } = require('cucumber');
+const webdriverio = require('webdriverio');
 
-When('I navigate to google.com', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+const options = {
+  desiredCapabilities: {
+    browserName: 'chrome'
+  }
+};
+
+When('I navigate to google.com', function (done) {
+  const driver = webdriverio.remote(options).init().call(done);
+  driver.end();
 });
 
 Then('I should be on the page google.com', function () {
