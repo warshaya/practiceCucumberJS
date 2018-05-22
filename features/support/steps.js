73b85f1e4b7e001a76049dc95af899d579e7005f
warshaya <warshaya@gmail.com>
2018-05-21 22:39:44 -0700

    +steps.js, stub in step definitions

---
+++ features/support/steps.js
@@ -0,0 +1,13 @@
+
+const { When, Then } = require('cucumber');
+
+When('I navigate to google.com', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should be on the page google.com', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
