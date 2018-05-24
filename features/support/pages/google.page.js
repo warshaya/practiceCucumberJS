4c35a671ab5cb0549acacce4dcae4925702ddf0c
warshaya <warshaya@gmail.com>
2018-05-24 08:16:12 -0700

    ~google.page.js, rewrite using es6, and supply getUrl to retrieve the current url

--- features/support/pages/google.page.js
+++ features/support/pages/google.page.js
@@ -1,10 +1,16 @@
 
+class googlePage { 
 
-function googlePage (driver) {
-  this.driver = driver;
-  this.driver.url('https://www.google.com');
-  this.driver.pause(10000).call();
-};
+  constructor (driver) {
+    this.driver = driver;
+    this.driver.url('https://www.google.com');
+  }
 
-module.exports = { googlePage };
+  getUrl () {
+    return this.driver.getUrl();
+  }
+
+}
+
+export { googlePage };
 
