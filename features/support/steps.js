8d383c0d726c0b8be730e588bde0ddf6db3567f1
warshaya <warshaya@gmail.com>
2018-05-24 08:18:20 -0700

    ~steps.js, rewrite imports using es6, use chai, use Before instead of CustomWorld, and use Promise to perform async assertion (fixes issue with tests finish before browser even opens)

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,30 +1,39 @@
 
-const { When, Then, setWorldConstructor, After } = require('cucumber');
-const webdriverio = require('webdriverio');
+import { When, Then, Before, After } from 'cucumber';
+import { remote } from 'webdriverio';
+import { expect } from 'chai';
 
-const { googlePage } = require('./pages/google.page');
+import { googlePage } from './pages/google.page';
 
-const options = {
-  desiredCapabilities: {
-    browserName: 'chrome'
+Before(function (done) {
+
+  const options = {
+    desiredCapabilities: {
+      browserName: 'chrome'
+    }
   }
-};
 
-function CustomWorld () {
-  this.driver = webdriverio.remote(options).init();
-}
+  this.driver = remote(options).init();
 
-setWorldConstructor(CustomWorld);
+});
 
 When('I navigate to google.com', function () {
-  const google = googlePage(this.driver);
+
+  this.googlepage = new googlePage(this.driver);
+
 });
 
 Then('I should be on the page google.com', function () {
 
+  return this.googlepage.getUrl().then(function (result) {
+    expect(result).to.equal('https://www.google.com/');
+  });
+
 });
 
 After(function () {
+ 
   this.driver.end();
+
 });
 
